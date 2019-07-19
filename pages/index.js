456dadac637a9a5a1eeadcc9fbdb7be9d4786754
Fiylex <mertbatuhanu@hotmail.com>
2019-07-19 16:23:01 +0300

    Update index.js
    Menü Ekleme

--- pages/index.js
+++ pages/index.js
@@ -1,11 +1,24 @@
 import { Header, Footer } from '../components/index';
 import '../styles/index.scss';
 
+const menu = [];
+
+function MenuEkleme() {
+  const text = window.document.getElementById('menu').value;
+  menu.push(text);
+  alert(menu);
+}
+
+// Mert Batuhan ÜNVERDİ
+
 function Home() {
   return (
     <div>
       <Header />
-      <div>Hello World</div>
+      <div>
+        <input type="text" id="menu" />
+        <input type="button" id="MenuEkle" value="Menü Ekle" onClick={MenuEkleme()} />
+      </div>
       <Footer />
     </div>
   );
